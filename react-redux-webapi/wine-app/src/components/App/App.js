34df988a7e5993b753ea16c18005bc2c9c445031
Anders Kofoed <anders.kfd@gmail.com>
2018-05-30 17:31:42 +0200

    Add starting point project

---
+++ react-redux-webapi/wine-app/src/components/App/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Wine List App</h1>
+        </header>
+        {/* Add list with wines fetched from api */}
+
+      </div>
+    );
+  }
+}
+
+export default App;
