5190d8e136bce597add3f087e82201727b2a4474
Sindre Nyborg Berge <snb@itverket.no>
2021-01-07 16:42:10 +0100

    fix(XSS attack): Replace use of innerHTML with textContent

--- bootcamp/web/VanillaJS/index.js
+++ bootcamp/web/VanillaJS/index.js
@@ -1,50 +1,50 @@
 function getUser() {
-  const username = document.getElementById('username-input').value;
+  const username = document.getElementById("username-input").value;
 
-  const userCard = document.getElementById('usercard');
-  const statusIndicator = document.getElementById('statusIndicator');
-  userCard.innerHTML = '';
+  const userCard = document.getElementById("usercard");
+  const statusIndicator = document.getElementById("statusIndicator");
+  userCard.textContent = "";
 
-  userCard.classList.remove('visible');
-  statusIndicator.classList.add('spin');
+  userCard.classList.remove("visible");
+  statusIndicator.classList.add("spin");
 
   fetch(`https://api.github.com/users/${username}`)
-    .then(response => {
-      statusIndicator.classList.remove('spin');
-      userCard.classList.add('visible');
+    .then((response) => {
+      statusIndicator.classList.remove("spin");
+      userCard.classList.add("visible");
 
       if (response.status === 404) {
-        userCard.innerHTML = `Unable to find user: ${username}`;
+        userCard.textContent = `Unable to find user: ${username}`;
         return;
       }
       if (response.status !== 200) {
-        response.json().then(function(responseJson) {
-          userCard.innerHTML(
-            `Looks like there was a problem. Status Code:  ${response.status}. Message: ${responseJson && responseJson.message} `
+        response.json().then(function (responseJson) {
+          userCard.textContent(
+            `Looks like there was a problem. Status Code:  ${response.status}. Message: ${
+              responseJson && responseJson.message
+            } `
           );
         });
 
         return;
       }
 
-      response.json().then(function(userData) {
-        const userName = document.createElement('h2');
-        userName.innerText = userData.login;
+      response.json().then(function (userData) {
+        const userName = document.createElement("h2");
+        userName.textContent = userData.login;
 
-        const homepageUrl = document.createElement('a');
+        const homepageUrl = document.createElement("a");
         homepageUrl.text = userData.blog;
         homepageUrl.href = userData.blog;
-        homepageUrl.style = 'display:block';
+        homepageUrl.style = "display:block";
 
-        const avatar = document.createElement('img');
+        const avatar = document.createElement("img");
         avatar.src = userData.avatar_url;
         avatar.height = 200;
         avatar.width = 200;
 
-        userCard.appendChild(userName);
-        userCard.appendChild(homepageUrl);
-        userCard.appendChild(avatar);
+        userCard.append(userName, homepageUrl, avatar);
       });
     })
-    .catch(() => statusIndicator.classList.remove('spin'));
+    .catch(() => statusIndicator.classList.remove("spin"));
 }
