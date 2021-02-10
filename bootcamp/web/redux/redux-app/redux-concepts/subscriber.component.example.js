2f75e5c1bada873653ba22460a789cdf0d48146f
Sindre Nyborg Berge <snb@itverket.no>
2021-02-10 19:23:05 +0100

    feat(Redux): Add one-way-data-flow example and simplify subscriber example

--- bootcamp/web/redux/redux-app/redux-concepts/subscriber.component.example.js
+++ bootcamp/web/redux/redux-app/redux-concepts/subscriber.component.example.js
@@ -1,23 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
+
+import { decrement, increment } from "./counterSlice";
 import { useDispatch, useSelector } from "react-redux";
 
-import styles from "./Counter.module.css";
-import { decrement, increment } from "./counterSlice";
-
 export function Counter() {
-  const count = useSelector((state) => state.counter.value);
+  const currentValue = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
 
   return (
-    <div className={styles.counterSection}>
-      <div className={styles.row}>
-        <button className={styles.button} aria-label="Increment value" onClick={() => dispatch(increment())}>
-          +
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button className={styles.button} aria-label="Decrement value" onClick={() => dispatch(decrement())}>
-          -
-        </button>
+    <div>
+      <div>Current value is {currentValue}</div>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
       </div>
     </div>
   );
