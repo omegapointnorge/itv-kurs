8fdd331f332199a1babd18786d0f5548470e1473
Sindre Nyborg Berge <snb@itverket.no>
2021-02-17 17:36:09 +0100

    feat(Redux): Update to pptx

---
+++ bootcamp/web/redux/redux-app/code-examples/equality.example.js
@@ -0,0 +1,18 @@
+const mary = {
+  name: "Mary Carter",
+  gender: "Female",
+  superhero: false,
+};
+
+const mary2 = {
+  name: "Mary Carter",
+  gender: "Female",
+  superhero: false,
+};
+
+console.log(mary === mary2); // false
+
+const five = 5;
+const alsoFive = 5;
+
+console.log(five === alsoFive);
