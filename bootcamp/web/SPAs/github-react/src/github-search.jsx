21c1c1f6653509ca0f961e7938999f70591b6b00
Sindre Nyborg Berge <snb@itverket.no>
2021-09-01 19:08:45 +0200

    Update styling for web app demo

--- bootcamp/web/SPAs/github-react/src/github-search.jsx
+++ bootcamp/web/SPAs/github-react/src/github-search.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const GithubSearch = () => {
-  const [username, setUserName] = useState('');
+  const [username, setUserName] = useState("");
   const [githubUser, setGithubUser] = useState({});
   const [isLoadingUser, setIsLoadingUser] = useState(false);
 
   const getUser = () => {
     setIsLoadingUser(true);
     fetch(`https://api.github.com/users/${username}`)
-      .then(response => {
-        response.json().then(githubUser => {
+      .then((response) => {
+        response.json().then((githubUser) => {
           setGithubUser(githubUser);
           setIsLoadingUser(false);
         });
@@ -18,29 +18,27 @@ const GithubSearch = () => {
   };
 
   return (
-    <div>
-      <h1>Hi, and welcome. Search by user below</h1>
-      <input onChange={e => setUserName(e.currentTarget.value)}></input>
-      <button onClick={() => getUser()}>Click me</button>
+    <main>
       <div>
-        {isLoadingUser ? <div className="spin"></div> : <React.Fragment />}
-        {githubUser.login ? (
-          <div>
-            <div className="card">
-              <h2>{githubUser.login}</h2>
-
-              <a href={githubUser.blog} style={{ display: 'block' }}>
-                {githubUser.blog}
-              </a>
-
-              {githubUser.avatar_url ? <img src={githubUser.avatar_url} alt="avatar" width="200" height="200" /> : <React.Fragment />}
-            </div>
+        <h1>Github users</h1>
+        <div>
+          <input onChange={(e) => setUserName(e.currentTarget.value)} autoFocus></input>
+          <button onClick={() => getUser()}>Search</button>
+        </div>
+      </div>
+      <div id="content">
+        {isLoadingUser && <div className="spin"></div>}
+        {githubUser.login && (
+          <div className="card">
+            <h2>{githubUser.login}</h2>
+            <a href={githubUser.blog} style={{ display: "block" }}>
+              {githubUser.blog}
+            </a>
+            {githubUser.avatar_url && <img src={githubUser.avatar_url} alt="avatar" width="200" height="200" />}
           </div>
-        ) : (
-          <React.Fragment />
         )}
       </div>
-    </div>
+    </main>
   );
 };
 
